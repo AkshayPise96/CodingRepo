e8d7e66021611c8c7178bcfa99ec1c6859deb214
AkshayPise1996 <akkipise131996@gmail.com>
2024-03-01 22:43:03 -0800

    first commit

---
+++ src/test/java/dev/Pise/productcatlogueservice/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Pise.productcatlogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
